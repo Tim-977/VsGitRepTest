7715f1845402ba0d452ecd956093c9b66a7960c4
Tim977 <gentim977@gmail.com>
2022-10-15 22:44:01 +0300

    This is the 1st commit

---
+++ main.cpp
@@ -0,0 +1,10 @@
+#include <iosream>
+
+using namespace std;
+
+int main(){
+    int a;
+    cin >> a;
+    cout << "This file has been loaded to my repository via vs code and git" << endl;
+    cout << "The number a is: " << a;
+}
