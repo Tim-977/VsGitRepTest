8d97d648ef16b6a009465267a7130c39252c5cae
Tim977 <gentim977@gmail.com>
2022-10-15 22:48:57 +0300

    The 2nd commit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,5 @@ int main(){
     int a;
     cin >> a;
     cout << "This file has been loaded to my repository via vs code and git" << endl;
-    cout << "The number a is: " << a << endl;
-    cout << 'bye!'
+    cout << "The number a is: " << a;
 }
